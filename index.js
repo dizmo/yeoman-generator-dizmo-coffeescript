7f39b799f3b8adadce16ebb0bbe5e5f31bd19142
Hasan Karahan <hasan.karahan@blackhan.com>
2019-06-13 18:32:13 +0300

    DIG-242: refactored index.js

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
 'use strict';
 
-let fs = require('fs'),
-    Generator = require('yeoman-generator'),
-    lodash = require('lodash'),
-    rimraf = require('rimraf');
-
-function sort(dictionary) {
-    let array = [],
-        sorted = {};
-
-    for(let key in dictionary) {
-        array[array.length] = key;
-    }
-    array.sort();
-
-    for(let i = 0; i < array.length; i++) {
-        sorted[array[i]] = dictionary[array[i]];
-    }
-    return sorted;
-}
+const fs = require('fs');
+const Generator = require('yeoman-generator');
+const lodash = require('lodash');
+const rimraf = require('rimraf');
 
 module.exports = class extends Generator {
     writing() {
-        let upgrade = Boolean(
-            this.options.upgrade && fs.existsSync('package.json'));
+        const upgrade = Boolean(
+            this.options.upgrade && fs.existsSync('package.json')
+        );
         if (!upgrade || upgrade) {
             this.fs.copy(
                 this.templatePath('gulp/'),
-                this.destinationPath('gulp/'));
+                this.destinationPath('gulp/')
+            );
         }
         if (!upgrade || upgrade) {
-            let pkg = this.fs.readJSON(
+            const pkg = this.fs.readJSON(
                 this.destinationPath('package.json')
             );
             pkg.devDependencies = sort(
@@ -46,23 +33,32 @@ module.exports = class extends Generator {
             delete pkg.devDependencies['gulp-eslint'];
             delete pkg['babel'];
             this.fs.writeJSON(
-                this.destinationPath('package.json'), pkg, null, 2);
+                this.destinationPath('package.json'), pkg, null, 2
+            );
         }
         if (!upgrade) {
             this.fs.copy(
                 this.templatePath('src/'),
-                this.destinationPath('src/'));
+                this.destinationPath('src/')
+            );
             this.fs.copy(
                 this.templatePath('coffeelint.json'),
-                this.destinationPath('coffeelint.json'));
+                this.destinationPath('coffeelint.json')
+            );
         }
         this.conflicter.force = this.options.force || upgrade;
     }
-
     end() {
         rimraf.sync(
-            this.destinationPath('.eslintrc.json'));
+            this.destinationPath('.eslintrc.json')
+        );
         rimraf.sync(
-            this.destinationPath('src/index.js'));
+            this.destinationPath('src/index.js')
+        );
     }
 };
+function sort(object) {
+    return Object.entries(object).sort().reduce(
+        (a, [k, v]) => { a[k] = v; return a; }, {}
+    );
+}
